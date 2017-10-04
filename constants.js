110222fd865966d7e076590efbbca0661daf139a
scarloxk <32519756+scarloxk@users.noreply.github.com>
2017-10-04 22:59:06 +0700

    Add files via upload
    00

---
+++ constants.js
@@ -0,0 +1,20 @@
+var ROLE_VILLAGER = 'muggle';
+var ROLE_WEREWOLF = 'deatheater';
+var ROLE_DOCTOR = 'healer';
+var ROLE_SEER = 'trewlaney';
+
+var TURN_LEECH = 'leech';
+var TURN_KILL = 'kill';
+var TURN_HEAL = 'heal';
+var TURN_REVEAL = 'reveal';
+
+module.exports = {
+    "ROLE_VILLAGER": 'muggle',
+    "ROLE_WEREWOLF": 'deatheater',
+    "ROLE_DOCTOR": 'healer',
+    "ROLE_SEER": 'trewlaney',
+    "TURN_LEECH": 'leech',
+    "TURN_KILL": 'kill',
+    "TURN_HEAL": 'heal',
+    "TURN_REVEAL": 'reveal'
+}
